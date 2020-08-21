fafca80357608c06815b77078f0e7d05391626f5
adn <abdind93@gmail.com>
2020-08-21 18:15:53 +0000

    first commit

---
+++ sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'cfpdrupaldb',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => 'adn_',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'yxKJ58xiT1LgmkBfAd4o7Yctt2_2siRLO80W9AwJd_JRXgkT1Ehd-_GHf87fFTX2JwOcI0d1RQ';
+$settings['config_sync_directory'] = 'sites/default/files/config_SCcbyj3uEaxNrUL-hb0LIuIA6If2NQ-2cM6NLd8Uo7yjZ1EsaEhx6LPn3sGFfjcLyd6T9sVwmA/sync';
